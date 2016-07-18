ec220e3b2778a85e3480eee00b05773382c1a416
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2016-07-18 21:44:20 +0200

    Using permanent output buffer

--- pack-get-data.lisp
+++ pack-get-data.lisp
@@ -5,13 +5,12 @@
 (defpackage #:git-api.pack.get-data
   (:use #:cl #:cl-annot.class #:alexandria #:git-api.utils #:static-vectors))
 
-
 (in-package #:git-api.pack.get-data)
 (annot:enable-annot-syntax)
 
-;; imports
-(from nibbles import read-ub32/be ub32ref/be read-ub64/be)
-(from babel import octets-to-string)
+
+@export
+(defparameter *try-use-temporary-output-buffer* t)
 
 
 (defparameter *temporary-read-buffer* (make-array 8192
@@ -21,9 +20,16 @@
 
 (defparameter *temporary-output-buffer* (make-array 8192
                                                     :element-type '(unsigned-byte 8)
-                                                    :fill-pointer 0))
-@export
-(defparameter *try-use-temporary-output-buffer* t)
+                                                    :fill-pointer 8192))
+
+
+(defparameter *temporary-static-read-buffer* (make-static-vector 8192))
+
+
+(defparameter *temporary-static-output-buffer* (make-static-vector 8192))
+
+(defvar *uncompressed-size-ptr* (cffi:foreign-alloc :unsigned-long))
+
 
 @export
 (defun get-object-data-old (offset compressed-size uncompressed-size stream)
@@ -55,31 +61,6 @@ BUFFER is a optional buffer to read compressed data"
 
 
 
-
-(defparameter *temporary-static-read-buffer* (make-static-vector 8192))
-
-
-(defparameter *temporary-static-output-buffer* (make-static-vector 8192))
-
-;;; (git-api.zlib-wrapper::uncompress
-;;;  (static-vectors:static-vector-pointer out)
-;;;  ptr-to-int
-;;;  (static-vectors:static-vector-pointer buff)
-;;;  (length buff))
-
-
-;; create unsigned long reference
-;;(setq ptr-to-int (cffi:foreign-alloc :unsigned-long))
-;; set value of this pointer
-;; (setf (cffi:mem-ref ptr-to-int :unsigned-long) (length out))
-;; get value of unsigned long reference
-;;(cffi:mem-ref ptr-to-int :unsigned-long)
-
-    
-;; get value of unsigned long reference
-;;(cffi:mem-ref ptr-to-int :unsigned-long)
-
-
 @export
 (defun get-object-data-general (offset compressed-size uncompressed-size stream)
   "Return the uncompressed data for pack-entry from the opened file stream.
@@ -108,37 +89,6 @@ BUFFER is a optional buffer to read compressed data"
     ;; uncompress chunk
     (zlib:uncompress read-buffer :output-buffer output-buffer :start 0 :end compressed-size)))
 
-@export
-(defun get-object-data-zlib (offset compressed-size uncompressed-size stream)
-  ;; move to position data-offset
-  (file-position stream offset)
-  (with-static-vector (input compressed-size)
-    (with-static-vector (output uncompressed-size)
-      (let ((output-buffer
-             (make-array uncompressed-size 
-                         :element-type '(unsigned-byte 8)))
-            (uncompressed-size-ptr
-             (cffi:foreign-alloc :unsigned-long)))
-        ;; set value of this pointer    
-        (setf (cffi:mem-ref uncompressed-size-ptr :unsigned-long) uncompressed-size)
-        ;; read the data
-        (read-sequence input stream :end compressed-size)
-        ;; uncompress chunk
-        (let* ((foreign-output (static-vector-pointer output))
-               (result
-                (git-api.zlib-wrapper::uncompress
-                 foreign-output
-                 uncompressed-size-ptr
-                 (static-vector-pointer input)
-                 compressed-size)))
-          (unless (= result 0)
-            (error (format nil "zlib::uncompress returned ~d" result)))
-          (loop for i from 0 below uncompressed-size
-                for val = (the (unsigned-byte 8) (cffi:mem-aref foreign-output :unsigned-char i))
-                do (setf (aref output-buffer i) val)))
-        output-buffer))))
-
-(defvar *uncompressed-size-ptr* (cffi:foreign-alloc :unsigned-long))
 
 @export
 (defun get-object-data (offset compressed-size uncompressed-size stream)
@@ -146,8 +96,13 @@ BUFFER is a optional buffer to read compressed data"
   (file-position stream offset)
   (let ((input *temporary-static-read-buffer*)
         (output *temporary-static-output-buffer*)
-        (output-buffer (make-array uncompressed-size 
-                                   :element-type '(unsigned-byte 8))))
+        (output-buffer
+         (if (and *try-use-temporary-output-buffer* (<= uncompressed-size 8192))
+             (progn
+               (setf (fill-pointer *temporary-output-buffer*) uncompressed-size)
+               *temporary-output-buffer*)
+             (make-array uncompressed-size 
+                         :element-type '(unsigned-byte 8)))))
     ;; set value of the pointer to size output buffer    
     (setf (cffi:mem-ref *uncompressed-size-ptr* :unsigned-long) uncompressed-size)
     (handler-case
